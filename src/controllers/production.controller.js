62aec83a675adec2003f24f23f21b845a5b2483e
Mario Serrano <maser84@gmail.com>
2022-10-31 00:05:32 -0500

    CRUD Production add

--- src/controllers/production.controller.js
+++ src/controllers/production.controller.js
@@ -1,26 +1,95 @@
 const { response, request } = require("express");
-const User = require("../models/user.model");
+const Production = require("../models/production.model");
 
 
 const getProductions = async (req = request, res = response) => {
-
+    try {
+        const productions = await Production.find();
+        res.status(200).json({
+            msg: 'Lista de producciones',
+            productions
+        })
+    } catch (err) {
+        return res.status(500).json({ message: err.message });
+    }
 }
 
 const createProduction = async (req = request, res = response) => {
+    try {
+        const body = req.body;
+        const production = new Production(body);
+
+        const productionSaved = await production.save();
+
+        res.status(201).json({
+            msg: 'Production Created',
+            productionSaved
+        })
+    } catch (err) {
+        return res.status(500).json({ message: err.message });
+    }
 
 }
 
 
 const getProductionById = async (req = request, res = response) => {
+    try {
+        const production = await Production.findById(req.params._id);
+        if (production != null) {
+            res.status(200).json({
+                msg: 'Producción por Id',
+                production
+            });
+        } else {
+            res.status(404).json({
+                msg: 'Material no encontrado, verifique el Id ingresado'
+            })
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 
 }
 
 const updateProductionById = async (req = request, res = response) => {
+    try {
+        const paramsId = req.params._id;
+        const body = req.body;
+        const updatedProduction = await Production.findByIdAndUpdate(paramsId, body, { new: true });
+        if (updatedProduction != null) {
+            res.status(200).json({
+                msg: 'Produccion actualizada segun Id',
+                updatedProduction
+            });
+        } else {
+            res.status(404).json({
+                msg: 'Id de produccion no encontrado'
+            })
+        }
+    }
+    catch (err) {
+        return res.status(500).json({ message: err.message });
+    }
 
 }
 
 const deleteProductionById = async (req = request, res = response) => {
-
+    try {
+        const paramsId = req.params._id;
+        const body = { deleted: true }
+        const deletedProduction = await Production.findByIdAndUpdate(paramsId, body);
+        if (deletedProduction != null) {
+            res.status(200).json({
+                msg: 'Material eliminado Id:' + paramsId
+            });
+        } else {
+            res.status(404).json({
+                msg: 'Material no encontrado, verifique el Id ingresado'
+            })
+        }
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
 }
 
 module.exports = {
