9533a3e25a0558868746efbef2b7cf44dbedba0c
Mario Serrano <maser84@gmail.com>
2022-12-03 21:39:18 -0500

    Configurando server

--- build/app.js
+++ build/app.js
@@ -4,5 +4,18 @@ require('dotenv').config();
 
 var Server = require('./models/server.model');
 
+var pkg = require('../package.json');
+
 var server = new Server();
 server.listen();
+server.app.set('pkg', pkg);
+console.log(pkg);
+server.app.get('/api', function (req, res) {
+  res.json({
+    app: server.app.get('pkg').name,
+    author: server.app.get('pkg').author,
+    description: server.app.get('pkg').description,
+    version: server.app.get('pkg').version,
+    url_docs: server.app.get('pkg').url_docs
+  });
+});
