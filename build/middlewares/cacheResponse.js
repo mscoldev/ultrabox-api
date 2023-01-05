714db01733f715595e038df228c1e241df0b4366
Mario Serrano <maser84@gmail.com>
2023-01-04 23:37:57 -0500

    Eliminado el cacheo de respuestas 201

--- build/middlewares/cacheResponse.js
+++ build/middlewares/cacheResponse.js
@@ -7,7 +7,7 @@ var cache = apicache.options({
     'cache-control': 'no-cache'
   },
   statusCodes: {
-    exclude: [404, 403, 500, 303, 401],
+    exclude: [201, 404, 403, 500, 303, 401],
     // list status codes to specifically exclude (e.g. [404, 403] cache all responses unless they had a 404 or 403 status)
     include: [200] // list status codes to require (e.g. [200] caches ONLY responses with a success/200 code)
 
--- src/middlewares/cacheResponse.js
+++ src/middlewares/cacheResponse.js
@@ -5,7 +5,7 @@ const cache = apicache.options({
         'cache-control': 'no-cache',
     },
     statusCodes: {
-        exclude: [404, 403, 500, 303, 401],             // list status codes to specifically exclude (e.g. [404, 403] cache all responses unless they had a 404 or 403 status)
+        exclude: [201, 404, 403, 500, 303, 401],             // list status codes to specifically exclude (e.g. [404, 403] cache all responses unless they had a 404 or 403 status)
         include: [200],             // list status codes to require (e.g. [200] caches ONLY responses with a success/200 code)
     },
     debug: true,
